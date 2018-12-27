1de9b85df92e58ec4eedeb445d6bf04c26bbf9cb
Jason Byer <jbyer3@gmail.com>
2018-12-27 15:58:44 -0500

    added routes controller and services for templates

--- routes/api.route.js
+++ routes/api.route.js
@@ -2,7 +2,9 @@ var express = require('express')
 
 var router = express.Router()
 var todos = require('./api/todos.route')
+var templates = require('./api/templates.route')
 
 router.use('/todos', todos);
+router.use('/templates', templates)
 
 module.exports = router;

c384196934e0cd3ffb72d01cf2ddee3d2dd96ff4
Jason Byer <jbyer3@gmail.com>
2018-12-29 23:54:01 -0500

    add words component and routing

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WordsComponent } from './words/words.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    WordsComponent
   ],
   imports: [
     BrowserModule,

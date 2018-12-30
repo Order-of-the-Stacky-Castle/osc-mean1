da21ebde6ad8a6a720aab766314941a81aac5563
Ben Nowak <bennnowak@gmail.com>
2018-12-30 00:09:45 -0500

    updated nav to use routerLink

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,

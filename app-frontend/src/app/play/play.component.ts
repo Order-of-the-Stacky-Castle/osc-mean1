bb4fcea14080942efa0c7005ebcf3a18a78a40d5
Ben Nowak <bennnowak@gmail.com>
2019-01-07 15:55:38 -0500

    Rework of play file

--- app-frontend/src/app/play/play.component.ts
+++ app-frontend/src/app/play/play.component.ts
@@ -12,10 +12,27 @@ export class PlayComponent implements OnInit {
   constructor(private playService: PlayService) { }
 
   templatesList: Template[];
+  showTemplateSelect: boolean;
+  currentTemplate: Template;
+  showGetWordsForm: boolean;
+  showMadLib: boolean;
   ngOnInit() {
     this.playService.getTemplates().subscribe( templates => {
       this.templatesList = templates.data.docs;
       console.log(templates);
     });
+    this.showTemplateSelect = true;
+    this.showGetWordsForm = false;
+    this.showMadLib = false;
+  }
+
+  getWords(id) {
+    for ( const template of this.templatesList) {
+      if ( template._id === id) {
+        this.currentTemplate = template;
+      }
+    }
+    this.showTemplateSelect = false;
+    this.showGetWordsForm = true;
   }
 }

1ea362fd08c8dd238efc6f31ba90c8370fc5a11a
Jason Byer <jbyer3@gmail.com>
2019-01-08 19:28:21 -0500

    playing a template saves words correctly

--- app-frontend/src/app/play/play.component.ts
+++ app-frontend/src/app/play/play.component.ts
@@ -4,6 +4,7 @@ import {PlayService} from '../services/play.service';
 import {FormControl, NgForm} from '@angular/forms';
 import { template } from '@angular/core/src/render3';
 import { WordService } from '../services/word.service';
+import { Word } from '../models/words.model';
 
 @Component({
   selector: 'app-play',
@@ -22,11 +23,12 @@ export class PlayComponent implements OnInit {
   showGetWordsForm: boolean;
   showMadLib: boolean;
   finishedStory = "";
+  newWord: Word = new Word();
 
   ngOnInit() {
     this.playService.getTemplates().subscribe( templates => {
       this.templatesList = templates.data.docs;
-      console.log(templates);
+      // console.log(templates);
     });
     this.showTemplateSelect = true;
     this.showGetWordsForm = false;
@@ -45,7 +47,7 @@ export class PlayComponent implements OnInit {
         this.wordList.push(x);
       }
     });
-    console.log(this.wordList);
+    // console.log(this.wordList);
     // for(let part of this.currentTemplate.body){
     //   console.log(part);
     // }
@@ -62,28 +64,49 @@ export class PlayComponent implements OnInit {
     Object.values(formWords).forEach(x => {
       enteredWords.push(x);
     })
-    console.log("THESEWORDS", enteredWords);
+    // console.log("THESEWORDS", enteredWords);
+    // console.log(this.wordList)
     // console.log("this is the template", this.currentTemplate.body);
     let i = 0;
     this.currentTemplate.body.forEach(x => {
       if(typeof x === 'object'){
         let word = enteredWords[i];
-        console.log('looketme',word)
+        // console.log('looketme',word)
         this.finishedStory += word;
         this.finishedStory += " ";
-        this.wordService.createWord(word)
-          .subscribe((res) => {
-            // this.wordsList.push(this.newWord)
-            // this.newWord = new Word()
-          });
+        
         i++
       } else {
         this.finishedStory += x.toString();
       }
     })
+    this.synthesizeWord(enteredWords, this.wordList)
   }
 
   // generateMadlib(x){
   //   console.log("madlib says", x.form.value)
   // }
+
+  synthesizeWord(word, details){
+    // console.log(word)
+    // console.log(details)
+    for(let i = 0; i < word.length; i++) {
+      const wordObject = {
+        "word": word[i],
+        "type": details[i].type.toString(),
+        "subtype": details[i].subType.toString(),
+        "_id": this.newWord._id
+      }
+      // console.log("word obj is ", wordObject)
+      // console.log("this details", this.wordList)
+      this.wordService.createWord(wordObject)
+        .subscribe((res) => {
+          // this.wordsList.push(this.newWord)
+          this.newWord = new Word()
+        });
+
+    }
+  }
+
+
 }
